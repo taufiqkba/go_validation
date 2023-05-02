d52bc190cecc0bbfde688ed162c5bc146248873f
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-05-02 11:13:22 +0700

    Validation collection

--- validation_test.go
+++ validation_test.go
@@ -143,3 +143,36 @@ func TestNestedStruct(t *testing.T) {
 		fmt.Println(err.Error())
 	}
 }
+
+// validation collection
+func TestCollection(t *testing.T) {
+	type Address struct {
+		City    string `validate:"required"`
+		Country string `validate:"required"`
+	}
+
+	type User struct {
+		Id      string    `validate:"required"`
+		Name    string    `validate:"required"`
+		Address []Address `validate:"required,dive"`
+	}
+	validate := validator.New()
+	request := User{
+		Id:   "",
+		Name: "",
+		Address: []Address{
+			{
+				City:    "",
+				Country: "",
+			},
+			{
+				City:    "",
+				Country: "",
+			},
+		},
+	}
+	err := validate.Struct(request)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
