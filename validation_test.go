7e3e38af30be5be225524607dfe15608c6cac08e
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-05-02 10:08:02 +0700

    Validation Variable

---
+++ validation_test.go
@@ -0,0 +1,25 @@
+package go_validation
+
+import (
+	"fmt"
+	"github.com/go-playground/validator/v10"
+	"testing"
+)
+
+func TestValidation(t *testing.T) {
+	var validate *validator.Validate = validator.New()
+	if validate == nil {
+		t.Error("Validate is nil")
+	}
+}
+
+func TestValidationVariable(t *testing.T) {
+	validate := validator.New()
+	user := "name"
+
+	err := validate.Var(user, "required")
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
